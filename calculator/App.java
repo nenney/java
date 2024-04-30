dcb2e0d89f44dd47acffac108decb52d3d139cc0
nenney <a01056499749@gmail.com>
2024-05-01 02:17:01 +0900

    result print

--- calculator/App.java
+++ calculator/App.java
@@ -13,6 +13,24 @@ public class App {
         int secondNumber = sc.nextInt(); // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
 
         System.out.print("사칙연산 기호를 입력하세요: ");
-        char operator = sc.next().charAt(0); // 사칙연산 기호를 입력받고 적합한 타입의 변수에 저장합니다.
+        char operator = sc.next().charAt(0); // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+
+        int result = 0; // 결과값 초기화
+
+        if (operator == '+') {
+            result = firstNumber + secondNumber;
+        } else if (operator == '-') {
+            result = firstNumber - secondNumber;
+        } else if (operator == '*') {
+            result = firstNumber * secondNumber;
+        } else if (operator == '/') {
+            result = firstNumber / secondNumber;
+        } else {
+            System.out.println("잘못된 연산 기호입니다.");
+            return; // 오류 발생 시 프로그램 종료
+        }
+
+        // 연산 결과 출력
+        System.out.println("결과: " + result);
     }
 }
