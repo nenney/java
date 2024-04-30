16026c8aa39cfa5c151aa634acd48eacfd168c08
nenney <a01056499749@gmail.com>
2024-05-01 02:04:17 +0900

    char operator

--- calculator/App.java
+++ calculator/App.java
@@ -2,7 +2,7 @@ package calculator;
 
 import java.util.Scanner;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -11,5 +11,8 @@ public class Main {
 
         System.out.print("두 번째 숫자를 입력하세요: ");
         int secondNumber = sc.nextInt(); // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0); // 사칙연산 기호를 입력받고 적합한 타입의 변수에 저장합니다.
     }
 }
